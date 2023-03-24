fef8f16049dc7fbb508aa18389d263ee8e37ed97
Biruk Kebede <biruk.key@gmail.com>
2023-03-24 23:05:27 +0000

    configuration, deployment  and verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,30 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
-task('accounts', 'Prints the list of accounts', async () => {
+task("accounts", "Prints the list of accounts", async () => {
 	const accounts = await ethers.getSigners()
 
 	for (const account of accounts) {
 		console.log(account.address)
 	}
 })
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
-  solidity: "0.8.18",
-};
+	defaultNetwork: "hardhat",
+	networks: {
+		sepolia: {
+			url: SEPOLIA_RPC_URL,
+			accounts: [PRIVATE_KEY],
+			chainId: 11155111,
+		},
+	},
+	solidity: "0.8.18",
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,42 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+	const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+	console.log("Deploying contract please wait ...")
+	const simpleStorage = await simpleStorageFactory.deploy()
+	await simpleStorage.deployed()
+	// Contract gets deployed to hardhat network by default
+	console.log(`Deployed contract to : ${simpleStorage.address}`)
+	if (network.config.chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
+		await simpleStorage.deployTransaction.wait(4)
+		await verify(simpleStorage.address, [])
+	}
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	const currentValue = await simpleStorage.retrieve()
+	console.log(`Current value is: ${currentValue}`)
+	const transactionResponse = await simpleStorage.store(7)
+	await transactionResponse.wait(1)
+	const updatedValue = await simpleStorage.retrieve()
+	console.log(`Updated value is: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+const verify = async (contractAddress, args) => {
+	console.log("Verifying contract ...")
+	try {
+		await run("verify:verify", {
+			address: contractAddress,
+			constructorArguments: args,
+		})
+	} catch (err) {
+		err.message.toLowerCase().includes("already verified")
+			? console.log("Already Verified!")
+			: console.log(err)
+	}
+}
+
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+	console.error(error)
+	process.exitCode = 1
+})
