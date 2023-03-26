77fb022c61e9a8711a9aa6369b6c512bf4a58ae2
Biruk Kebede <biruk.key@gmail.com>
2023-03-26 15:26:23 +0100

    modified hardhat.config and added a task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/blockNumber")
 
 /** @type import('hardhat/config').HardhatUserConfig */
-task("accounts", "Prints the list of accounts", async () => {
-	const accounts = await ethers.getSigners()
-
-	for (const account of accounts) {
-		console.log(account.address)
-	}
-})
+// task("accounts", "Prints the list of accounts", async () => {
+// 	const accounts = await ethers.getSigners()
+// 	for (const account of accounts) {
+// 		console.log(account.address)
+// 	}
+// })
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -21,6 +21,11 @@ module.exports = {
 			url: SEPOLIA_RPC_URL,
 			accounts: [PRIVATE_KEY],
 			chainId: 11155111,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545/",
+			// accounts will automatically picked by hardhat
+			chainId: 31337, // hardhat chainId
 		},
 	},
 	solidity: "0.8.18",
